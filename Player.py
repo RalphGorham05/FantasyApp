413035404a6a5ef5f35663279e583cab21151676
RalphGorham05 <Rgorham1@gaels.iona.edu>
2015-02-19 16:42:54 -0500

    more work on Player class

--- Player.py
+++ Player.py
@@ -1,3 +1,6 @@
+from mechanize import Browser
+from bs4 import BeautifulSoup
+
 class Player:
     def __init__(self, name):
         self.name = name
@@ -6,8 +9,48 @@ class Player:
         self.stats = []
 
 
+    city_list = []
+team_list = []
+abr_list = []
+#check to find team
+def get_Team(player, divs):
+    tName = ''
+    pdict = {}
+
+    for division in divs:
+        for team in division:
+            code = process_Site(team)
+            playerTable = code.find('table')
+            
+
+            #make list of all the cities
+            city_list.append(city_Name(team))
+            #make list of all the team
+            team_list.append(team_Name(team))
+            #make list of abbrs
+            abr_list.append(city_Abbr(team))
+            
+            
+            rows = playerTable.findChildren('tr')[2:]
+            for row in rows:
+                data = row.findChildren('td')
+                name = data[1].text
+               
+                pi = get_pID(name, team)
+                #pdict[name] = pi
+
+                #for k, v in pdict.iteritems():
+                    #print k,v 
+                    #Players.create(name=k, pid=v)
+                
+                if player == name:
+                    tName = city_Name(team)
+
+    return tName
+
+
     #returns player espn id
-    def get_pID(self, player, url):
+    def get_pID(self, url):
         br = Browser()
         site = br.open(url)
         page = site.read()
@@ -16,16 +59,16 @@ class Player:
         links = table.findAll('a')
     
         for link in links:
-            if player in link.text:
+            if self.name in link.text:
                 sep = link.get('href').split('/')
                 pid = sep[len(sep)-2]
 
         self.pid = pid
 
 
-    def get_Stats(self, pid, table_num = 3):
+    def get_Stats(self, table_num = 3):
         url = 'http://espn.go.com/nba/player/_/id/'
-        url += str(pid)
+        url += str(self.pid)
         br = Browser()
         site = br.open(url)
         page = site.read()
@@ -41,6 +84,11 @@ class Player:
             name = data
             for n in name:
                 self.stats.append(n)
-            
+
+
+
+p = Player('Brandon Bass')
+p.get_pID('http://espn.go.com/nba/team/roster/_/name/bos/boston-celtics')
+print p.pid
 
         
